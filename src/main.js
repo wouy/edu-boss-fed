a5dd489a23bee06c42a44572eb7c41cf7699bbfa
wujing <514827502@qq.com>
2021-06-19 20:13:15 +0800

    01

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 Element
+import ElementUI from 'element-ui'
+// 引入 Element 的主题文件
+// import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.scss'
+
+// 将Element 注册为Vue插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
